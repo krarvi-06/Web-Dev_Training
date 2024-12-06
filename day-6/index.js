4ee2e664b30c04e2add0a156cf1f2aba500852e5
krarvi-06 <arvindkr2901@gmail.com>
2024-12-06 15:55:46 +0530

    Add files via upload

---
+++ day-6/index.js
@@ -0,0 +1,78 @@
+// console.log("-----Welcome in JS------");
+// // Variables: var, let, const:
+// document.write("hello world");
+// // console.log(a);
+// let a = 5;
+// console.log(b);
+// var b = 5;
+// const c = 10;
+// console.log(c);
+// a = 9;
+// b = 5;
+// // c = 9;
+// a = "abc";
+// console.log(a);
+// if (a === 5) {
+//   console.log("hello world im if conditon");
+// } else {
+//   console.log("else condition");
+// }
+// Loops: for and while:
+// 9 * 1 = 9
+// 9 * 2 = 18
+// ...
+// const userInput = 99;
+// for (let i = 1; i <= 10; i++) {
+//   const tableRow = userInput + " * " + i + " = " + userInput * i;
+//   console.log(tableRow);
+// }
+// Questions:
+// Print table of N:
+// Print all number : Divisible by 3
+// Print all number : Divisible by 3 and 5
+// Print all number : Divisible by [(3 and 5) OR 7]
+// Print Even number and odd number
+// Print Sum of N numbers
+// Print Average of N numbers
+// for (let i = 1; i <= 100; i++) {
+//   if (i % 3 === 0) {
+//     if (i % 5 === 0) {
+//       console.log("-----value--------:", i);
+//     }
+//   }else if(i%9 === 0){
+//     console.log("-----value--------:", i);
+//   }
+// }
+// print Even numbers:
+// for (let i = 0; i < 100; i++) {
+//   if (i % 2 === 0) {
+//     console.log("----Even Number----");
+//   }
+// }
+// // print Odd numbers:
+// for (let i = 0; i < 100; i++) {
+//   if (i % 2 === 1) {
+//     console.log("----Odd Number----");
+//   }
+// }
+// While Loop:
+// let i = 0;
+// while (i < 10) {  /// 4 < 10 | 8 < 10 | 12 < 10
+//   console.log(i); // 0  4  8
+//   i=i+4;  // 0  4  8  12
+// }
+//  Questions:
+// Print Even Number: using while loop
+// print : [ - ] at position : divisible by 3
+// print number in reverse order : [n to 0]
+// print sum of all even numbers :
+// print no of digits in number N: eg : N = 1526  : ans 4
+// print number after replacing 5 to 9 : eg : no = 1526 : ans=1926
+// let ans = 0;
+// for (let i = 0; i < 100; i++) {
+//   if (i % 2 === 0) {
+//     // console.log("----Even Number----");
+//     ans = ans + i;
+//   }
+// }
+// console.log(ans);
