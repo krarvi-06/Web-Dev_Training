d12e008d78f6e45f136515b085266ff2b14dfffc
krarvi-06 <arvindkr2901@gmail.com>
2024-12-10 13:09:27 +0530

    Add files via upload

---
+++ day-10/index.js
@@ -0,0 +1,42 @@
+// const counter = document.getElementById("counter");
+// let count =0;
+// function incrementValue(){
+//     count++;
+//     counter.innerText = count;
+// }
+// function decValue(){
+//     count--;
+//     counter.innerText = count;
+// }
+
+
+
+// 2nd part of the code 
+//j.s always work line-by-line.
+const a = 5;
+const b = 9;
+try{
+console.log("value",a);
+a = 6;
+} catch(error){
+    console.log(error);
+}
+console.log("vaule of a =",a)
+
+// niche wale code ko chalane ke liye
+//  error handle karna padta hai 
+// solve karne ke liye line ko try cach mein daal denge
+
+console.log("---value of b---",b)
+
+// const myPromises = new Promises((resolve,reject)=>{}) 
+    const myPromise = my Promise(solve);
+    function solve(fun1, fun2){
+        //code likhte hai yaha (source code)
+        n=99999999
+        while(n--);
+        fun1("Data");
+    }
+    console.log("-------A------");
+    console.log(myPromise)
+    console.log("-------B------");
